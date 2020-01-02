30a557f365af4ae83d32a4d968dcb6c6b5ab256b
pyavchik <pyavchik@gmail.com>
2020-01-02 19:52:37 +0200

    added securing for components with route guard

--- frontend/todo/src/app/service/route-guard.service.ts
+++ frontend/todo/src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {HardcodedAuthenticationService} from './hardcoded-authentication.service';
 
@@ -8,13 +8,15 @@ import {HardcodedAuthenticationService} from './hardcoded-authentication.service
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) {
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+              private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
       return true;
     } else {
+      this.router.navigate(['login']);
       return false;
     }
   }
