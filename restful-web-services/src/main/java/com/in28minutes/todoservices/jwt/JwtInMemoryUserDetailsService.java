92caec969bdb743a3cfefd63d73831929379db9f
pyavchik <pyavchik@gmail.com>
2020-01-12 15:34:48 +0200

    connected POST, PUT and DELETE REST APIs to JPA Repository

--- restful-web-services/src/main/java/com/in28minutes/todoservices/jwt/JwtInMemoryUserDetailsService.java
+++ restful-web-services/src/main/java/com/in28minutes/todoservices/jwt/JwtInMemoryUserDetailsService.java
@@ -15,8 +15,12 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
   static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
   static {
+    inMemoryUserList.add(new JwtUserDetails(1L, "username",
+            "$2a$10$68nx1DBpcG71o9nAsFjzWuL0gOsNMWQTQdE3NgvDuU6EggtFeQJQu", "ROLE_USER_2"));
     inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
         "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+    inMemoryUserList.add(new JwtUserDetails(1L, "ranga",
+            "$2a$10$h.sPRpTB/4ks0js7tuEHJuwo4WhbnKZQkyupuj7BE6TkztgChT7lq", "ROLE_USER_2"));
   }
 
   @Override
